b7f95fcf683e2c0946a775ed197031f36b9a5213
Denis Gaivoronskii <denis.gaivoronskii@stel.global>
2024-01-11 10:34:25 +0700

    test added

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# retrybackoff
-Retry mechanism with exponential backoff and jitter
+# GoRetryBackoff
+
+GoRetryBackoff is a Go package providing a robust retry mechanism with exponential backoff and jitter. It's designed to handle transient failures in distributed systems, enhancing stability and performance in Go applications.
+
+## Features
+
+- **Exponential Backoff:** Increases the delay between retries exponentially.
+- **Jitter:** Adds randomness to the backoff intervals to prevent thundering herd problem.
+- **Customizable:** Easily configure max retries and max backoff duration.
+
+## Installation
+
+```bash
+go get github.com/SomchaiSPB/retrybackoff
+```
+
+## Usage
+
+```go
+package main
+
+import (
+    retrier "github.com/SomchaiSPB/retrybackoff"
+    "log"
+    "net/http"
+    "time"
+)
+
+func main() {
+    operation := func() error {
+        // Replace with your operation
+        _, err := http.Get("http://example.com")
+        return err
+    }
+
+    err := retrier.RetryWithBackoff(operation, 5, 2*time.Minute)
+    if err != nil {
+        log.Fatalf("Operation failed: %v", err)
+    }
+}
+```
--- retrier_test.go
+++ retrier_test.go
@@ -1,7 +1,83 @@
 package retrybackoff
 
-import "testing"
+import (
+	"fmt"
+	"math"
+	"testing"
+	"time"
+)
+
+func mockOperation(retryUntilSuccess int, timestamps *[]time.Time) func() error {
+	counter := 0
+	return func() error {
+		*timestamps = append(*timestamps, time.Now())
+		if counter < retryUntilSuccess {
+			counter++
+			return fmt.Errorf("error")
+		}
+		return nil
+	}
+}
 
 func TestRetrier(t *testing.T) {
+	type testCase struct {
+		name            string
+		operation       func() error
+		maxRetries      int
+		maxBackoff      time.Duration
+		wantErr         bool
+		expectedRetries int
+		timestamps      []time.Time
+	}
 
+	tests := []testCase{
+		{
+			name:            "Success on first try",
+			operation:       nil,
+			maxRetries:      3,
+			maxBackoff:      2 * time.Second,
+			wantErr:         false,
+			expectedRetries: 0,
+		},
+		{
+			name:            "Success after retries",
+			operation:       nil,
+			maxRetries:      3,
+			maxBackoff:      2 * time.Second,
+			wantErr:         false,
+			expectedRetries: 2,
+		},
+		{
+			name:            "Error after retries",
+			operation:       nil,
+			maxRetries:      2,
+			maxBackoff:      2 * time.Second,
+			wantErr:         true,
+			expectedRetries: 2,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.operation = mockOperation(tc.expectedRetries, &tc.timestamps)
+
+			err := RetryWithBackoff(tc.operation, tc.maxRetries, tc.maxBackoff)
+
+			if (tc.wantErr && err == nil) || (!tc.wantErr && err != nil) {
+				t.Errorf("Expected error: %v, got: %v", tc.wantErr, err)
+			}
+
+			for i := 1; i < len(tc.timestamps); i++ {
+				actualBackoff := tc.timestamps[i].Sub(tc.timestamps[i-1])
+				expectedBackoff := time.Duration(math.Pow(2, float64(i-1))) * time.Second
+				if expectedBackoff > tc.maxBackoff {
+					expectedBackoff = tc.maxBackoff
+				}
+
+				if actualBackoff < expectedBackoff {
+					t.Errorf("Backoff time too short. Retry %d: expected at least %v, got %v", i, expectedBackoff, actualBackoff)
+				}
+			}
+		})
+	}
 }
